5105b5647168ad68720bf40384217f80307ba193
ArcaneCipher <desjardins.jarp@gmail.com>
2024-09-29 18:45:30 -0400

    created joinList function

--- joinList.js
+++ joinList.js
@@ -10,9 +10,36 @@
  */
 
 // Write our function (we must define it too!) below
-// ...
+function joinList(array) {
+  if (array.length === 0) return "";
+
+  let joinString = "";
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== undefined && array[i] !== null && array[i] !== "") {
+      let value = String(array[i]);
+      if (joinString === "") {
+        joinString += value;
+      } else {
+        joinString += ", " + value;
+      }
+    }
+  }
+
+  return joinString;
+}
 
 // Test / Driver Code below...
 const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
 const concepts = joinList(conceptList);
 console.log(`Today I learned about ${concepts}.`);
+
+// Empty array test
+const emptyList = [];
+const blankConcepts = joinList(emptyList);
+console.log(`Today I learned about ${blankConcepts}.`);
+
+// Test with non-string values
+const mixedList = [42, true, null, "hello", "", undefined, "world"];
+const mixedConcepts = joinList(mixedList);
+console.log(`Today I learned about ${mixedConcepts}.`);
